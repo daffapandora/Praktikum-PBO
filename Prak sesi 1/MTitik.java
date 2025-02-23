3e4d13780936d8f4bc9d04feb159e7e37e9ee156
daffapandora <163204909+daffapandora@users.noreply.github.com>
2025-02-23 16:00:21 +0700

    Update MTitik.java

--- Prak sesi 1/MTitik.java
+++ Prak sesi 1/MTitik.java
@@ -1,32 +1,66 @@
+/*Nama File = MTitik.java
+ *Deskripsi = Berisi atribut dan method dalam class Titik
+ *Pembuat   = Daffa Pandora El-farisin/24060123140185
+ *Tanggal   = 18 Februari 2025
+*/
 public class MTitik {
     public static void main(String[] args) {
-        // Create objects
-        Titik t1 = new Titik();
-        t1.setAbsis(1.0);
-        t1.setOrdinat(2.0);
+        System.out.println("==========Membuat Objek Titik==========");
+        // Membuat titik T1
+        System.out.println("Titik T1: ");
+        Titik T1 = new Titik();
+        T1.setAbsis(3);
+        T1.setOrdinat(4);
+        T1.printTitik();
+        T1.geser(3, 4);
+        T1.printTitik();
+        System.out.println("Berada di kuadran: " + T1.getKuadran());
+        System.out.println("Jarak ke pusat: " + T1.getJarakPusat());
+
+
+        // Membuat Titik T2
+        System.out.println("\n");
+        System.out.println("Titik T2: ");
+        Titik T2 = new Titik();
+        T2.setAbsis(-10);
+        T2.setOrdinat(10);
+        T2.printTitik();
+        T2.geser(5, 5);
+        T2.printTitik();
+        System.out.println("Berada di kuadran: " + T2.getKuadran());
+        System.out.println("Jarak ke pusat: " + T2.getJarakPusat());
+
+        // Menghitung jarak antara T1 dan T2
+        System.out.println("\n");
+        System.out.println("==========Menghitung jarak antara T1 dan T2==========");
+        double jarakT1T2 = T1.getJarak(T2);
+        System.out.println("Jarak antara T1 dan T2: " + jarakT1T2);
+
+        // Membuat Titik T3
+        System.out.println("\n");
+        System.out.println("Titik T3: ");
+        Titik T3 = new Titik(-5, -3);
+        T3.printTitik();
+        T3.geser(3, -5);
+        T3.printTitik();
+        System.out.println("Berada di kuadran: " + T3.getKuadran());
+        System.out.println("Jarak ke pusat: " + T3.getJarakPusat());
+
+        // Membuat Titik T4
+        System.out.println("\n");
+        System.out.println("Titik T4: ");
+        Titik T4 = new Titik(8, 5);
+        T4.printTitik();
+
+        System.out.println("Jumlah objek Titik = " + Titik.getCounterTitik());
+    
+        System.out.println("\n==========Testing reflection methods==========");
+        System.out.println("Original T2: (" + T2.getAbsis() + ", " + T2.getOrdinat() + ")");
+        T2.refleksiX();
+        System.out.println("After refleksiX: (" + T2.getAbsis() + ", " + T2.getOrdinat() + ")");
         
-        Titik t2 = new Titik(3.0, 4.0);
-        
-        // Part 2 - Object Reference
-        Titik t3 = t1;
-        t3.setAbsis(5.0);
-        t3.setOrdinat(6.0);
-        
-        // Print results
-        System.out.println("T1: (" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
-        System.out.println("T2: (" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
-        System.out.println("T3: (" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
-        
-        // Print counter
-        System.out.println("Jumlah objek titik: " + Titik.getCounterTitik());
-        
-        // Test reflection methods
-        System.out.println("\nTesting reflection methods:");
-        System.out.println("Original T2: (" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
-        t2.refleksiX();
-        System.out.println("After refleksiX: (" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
-        
-        Titik t2ReflectedY = t2.getRefleksiY();
-        System.out.println("New point from getRefleksiY: (" + t2ReflectedY.getAbsis() + ", " + t2ReflectedY.getOrdinat() + ")");
+        Titik T2ReflectedY = T2.getRefleksiY();
+        System.out.println("New point from getRefleksiY: (" + T2ReflectedY.getAbsis() + ", " + T2ReflectedY.getOrdinat() + ")");
+    
     }
 }
